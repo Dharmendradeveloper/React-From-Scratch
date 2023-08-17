29b22670fd6a7ff419a63c0a7b5adea61bb1508a
Dharmendra Kumar <kumardakdharmendra@gmail.com>
2023-08-17 08:28:03 +0530

    Day_1

---
+++ App.js
@@ -0,0 +1,11 @@
+const heading = React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child1"},[
+    React.createElement("h1",{},"Hellow React in H1 Tag"),
+    React.createElement("h2",{},"Hello React in H2 Tag"),
+    React.createElement("h3",{},"Hello React in H3 Tag")
+]),
+React.createElement("child2",{},[
+    React.createElement("h4",{},"Hello React in H4 Tag"),
+    React.createElement("h5",{},"Hello in React H5 Tag")
+])])
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
